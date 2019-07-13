8277d1b8bf2f216de376472f8f61ca3ed89f4a7c
Spidergears <spidergears@users.noreply.github.com>
2019-07-13 09:42:18 +0530

    otp behaviour

--- metex/lib/worker.ex
+++ metex/lib/worker.ex
@@ -1,40 +1,68 @@
 defmodule Metex.Worker do
-  def loop do
-    receive do
-      {sender_pid, location} ->
-        send(sender_pid, {:ok, temperature_of(location)})
+  use GenServer
+
+  ## Client API
+  def start_link(opts \\[]) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  def get_temperature(pid, location) do
+    GenServer.call(pid, {:location, location})
+  end
+
+  def stop(pid) do
+    GenServer.cast(pid, :stop)
+  end
+
+  ## Sever callbacks
+  def init(:ok) do
+    {:ok, %{}}
+  end
+
+  def handle_call({:location, location}, _from, stats) do
+    case temperature_of(location) do
+      {:ok, temp} ->
+        {:reply, "#{temp} ˚C", stats}
       _ ->
-        IO.puts "don't know how to process this message"  
+        {:reply, :error, stats}
     end
-    loop()
+  end
+
+  def handle_cast(:stop, stats) do
+    {:stop, :normal, stats}
+  end
+
+  def handle_info(msg, stats) do
+    IO.puts "received #{inspect msg}"
+    {:noreply, stats}
+  end
+
+  def terminate(reason, stats) do
+    # We could write to a file, database etc
+    IO.puts "server terminated because of #{inspect reason}"
+       inspect stats
+    :ok
   end
 
   def temperature_of(location) do
-    result = url_for(location) 
+    url_for(location)
       |> HTTPoison.get
       |> parse_response
-    
-    case result do
-      {:ok, temprature} ->
-        "#{location}: #{temprature} ˚C"
-      :error ->
-        "#{location} not found"
-    end
   end
-  
+
   defp url_for(location) do
     location = URI.encode(location)
     "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{apikey()}"
   end
-  
+
   defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
     body |> JSON.decode! |> compute_temprature
   end
-  
+
   defp parse_response(_) do
     :error
   end
-  
+
   defp compute_temprature(json) do
     try do
       temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
@@ -43,21 +71,10 @@ defmodule Metex.Worker do
       _ -> :error
     end
   end
-  
+
   defp apikey, do: "ff8b38f17bb390d6decb52d7b77ed4c1"
 end
 
-## Usage
-# iex -S mix
-# Metex.Worker.temperature_of "Delhi"
-
-## Working with multiple cities
-# iex -S mix
-# cities = ["Delhi", "Mumbai", "Kolkata", "Chennai"]
-# cities |> Enum.map(fn city -> Metex.Worker.temperature_of(city) end)
-
-## Message Passing
-# cities |> Enum.each(fn city ->
-#   pid = spawn(Metex.Worker, :loop, [])
-#   send(pid, {self, city})
-# end)
+# Usage
+# {:ok, pid} = Metex.Worker.start_link
+# Metex.Worker.get_temperature(pid, "Delhi")
